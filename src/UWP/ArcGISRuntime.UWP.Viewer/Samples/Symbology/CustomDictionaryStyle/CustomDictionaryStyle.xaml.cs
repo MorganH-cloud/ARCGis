a5eb9f781c4195f3f4977b3dfc38fcf5e3b6bb60
Thad <ttilton@esri.com>
2019-07-30 22:43:23 -0700

    WPF and UWP versions of the custom dictionary renderer sample.

--- src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
@@ -8,17 +8,14 @@
 // language governing permissions and limitations under the License.
 
 using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Linq;
-using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ArcGISRuntime.Samples.Managers;
+using Windows.UI.Xaml;
 
 namespace ArcGISRuntime.UWP.Samples.CustomDictionaryStyle
 {
@@ -30,14 +27,132 @@ namespace ArcGISRuntime.UWP.Samples.CustomDictionaryStyle
     [ArcGISRuntime.Samples.Shared.Attributes.OfflineData("751138a2e0844e06853522d54103222a")]
     public partial class CustomDictionaryStyle
     {
+        // The custom dictionary style for symbolizing restaurants.
+        private DictionarySymbolStyle _restaurantStyle;
+
         public CustomDictionaryStyle()
         {
             InitializeComponent();
             Initialize();
         }
 
-        private void Initialize()
+        private async Task Initialize()
         {
+            try
+            {
+                // Open the custom style file.
+                string stylxPath = GetStyleFilePath();
+                _restaurantStyle = await DictionarySymbolStyle.CreateFromFileAsync(stylxPath);
+
+                // Create a new map with a streets basemap.
+                Map map = new Map(Basemap.CreateStreetsVector());
+
+                // Create the restaurants layer and add it to the map.
+                FeatureLayer restaurantLayer = new FeatureLayer(new Uri("https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Redlands_Restaurants/FeatureServer/0"));
+                map.OperationalLayers.Add(restaurantLayer);
+
+                // Get the fields from the restaurant feature table.
+                var restaurantTable = restaurantLayer.FeatureTable;
+                await restaurantTable.LoadAsync();
+                IReadOnlyList<Field> datasetFields = restaurantLayer.FeatureTable.Fields;
+
+                // Build a list of numeric and text field names.
+                var symbolFields = new List<string> { " " };
+                foreach (Field fld in datasetFields)
+                {
+                    if (fld.FieldType != FieldType.Blob &&
+                        fld.FieldType != FieldType.Date &&
+                        fld.FieldType != FieldType.Geometry &&
+                        fld.FieldType != FieldType.GlobalID &&
+                        fld.FieldType != FieldType.Guid &&
+                        fld.FieldType != FieldType.OID &&
+                        fld.FieldType != FieldType.Raster)
+                    {
+                        symbolFields.Add(fld.Name);
+                    }
+                }
+
+                // Show the fields in the combo boxes.
+                FoodStyleComboBox.ItemsSource = symbolFields;
+                RatingComboBox.ItemsSource = symbolFields;
+                PriceComboBox.ItemsSource = symbolFields;
+                HealthGradeComboBox.ItemsSource = symbolFields;
+                NameComboBox.ItemsSource = symbolFields;
+
+                // Select the default values for the expected symbol attributes.
+                FoodStyleComboBox.SelectedValue = "Style";
+                RatingComboBox.SelectedValue = "Rating";
+                PriceComboBox.SelectedValue = "Price";
+                HealthGradeComboBox.SelectedValue = " ";
+                NameComboBox.SelectedValue = "Name";
+
+                // Set the map's initial extent to that of the restaurants.
+                map.InitialViewpoint = new Viewpoint(restaurantLayer.FullExtent);
+
+                // Set the map to the map view.
+                MyMapView.Map = map;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("**Exception: " + ex.ToString());
+            }
+        }
+
+        public void ApplyDictionaryRendererClick(object sender, RoutedEventArgs e)
+        {
+            // Create overrides for expected field names that are different in this dataset.
+            var styleToFieldMappingOverrides = new Dictionary<string, string>
+            {
+                { "style", FoodStyleComboBox.SelectedValue.ToString() },
+                { "healthgrade", HealthGradeComboBox.SelectedValue.ToString() },
+                { "rating", RatingComboBox.SelectedValue.ToString() },
+                { "price", PriceComboBox.SelectedValue.ToString() },
+                { "opentimesun", "opensun" },
+                { "closetimesun", "closesun" },
+                { "opentimemon", "openmon" },
+                { "closetimemon", "closemon" },
+                { "opentimetue", "opentue" },
+                { "closetimetue", "closetue" },
+                { "opentimewed", "openwed" },
+                { "closetimewed", "closewed" },
+                { "opentimethu", "openthu" },
+                { "closetimethu", "closethu" },
+                { "opentimefri", "openfri" },
+                { "closetimefri", "closefri" },
+                { "opentimesat", "opensat" },
+                { "closetimesat", "closesat" }
+            };
+
+            // Create overrides for expected text field names (if any).
+            string labelField = NameComboBox.SelectedValue != null ? NameComboBox.SelectedValue.ToString() : "";
+            Dictionary<string, string> textFieldOverrides = new Dictionary<string, string>
+                {
+                    { "name", labelField }
+                };
+
+            // Set the text visibility configuration setting.
+            _restaurantStyle.Configurations.ToList().Find(c => c.Name == "text").Value = ShowTextCheckbox.IsChecked == true ? "ON" : "OFF";
+            
+            // Create the dictionary renderer with the style file and the field overrides.
+            DictionaryRenderer dictRenderr = new DictionaryRenderer(_restaurantStyle, styleToFieldMappingOverrides, textFieldOverrides);
+
+            // Apply the dictionary renderer to the layer.
+            var restaurantLayer = MyMapView.Map.OperationalLayers.First() as FeatureLayer;
+            restaurantLayer.Renderer = dictRenderr;
+        }
+
+        public void ApplySimpleRendererClick(object sender, RoutedEventArgs e)
+        {
+            // Apply a simple renderer that shows all points with the same marker symbol.
+            SimpleMarkerSymbol markerSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.Black, 12);
+            SimpleRenderer rendrr = new SimpleRenderer(markerSymbol);
+            var restaurantLayer = MyMapView.Map.OperationalLayers.First() as FeatureLayer;
+            restaurantLayer.Renderer = rendrr;
+        }
+
+        private static string GetStyleFilePath()
+        {
+            return DataManager.GetDataFolder("751138a2e0844e06853522d54103222a", "Restaurant.stylx");
         }
     }
 }
--- src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Symbology/CustomDictionaryStyle/readme.md
+++ src/WPF/ArcGISRuntime.WPF.Viewer/Samples/Symbology/CustomDictionaryStyle/readme.md
@@ -1,1 +1,40 @@
-# Basic readme template
+# Custom dictionary style
+
+Use a custom dictionary style (.stylx) to symbolize features using a variety of attribute values.
+
+![Custom dictionary style](custom-dictionary-style.jpg)
+
+## Use case
+
+When symbolizing geoelements in your map, you might need to convey several pieces of information with a single symbol. You could try to symbolize such data using a unique value renderer, but as the number of fields and values increases, that approach becomes impractical. With a dictionary renderer you can build each symbol on-the-fly based on one or several attribute values and handle a nearly infinite number of unique combinations.
+
+In this example, a restaurant layer with attributes for rating, style, health score, and open hours is symbolized using a dictionary renderer that displays a single symbol for all of these variables. 
+
+## How to use the sample
+
+Run the app to see a restaurants layer displayed with a simple marker symbol for all features. Tap "Apply renderer" to apply a dictionary renderer to the layer. The renderer uses symbols from a custom dictionary style to show unique symbols based on several feature attributes. Change the fields used for the renderer by specifying the `inspection` field for the expected `healthgrade` attribute or by choosing to use the alternate `rating` from the field called `MyRating`. You can also turn symbol text on, choose a field to display, and re-apply the renderer. Clear the renderer to return to the symbol marker symbol display.
+
+## How it works
+
+1. Create a new `DictionarySymbolStyle` by passing in the path to the custom style (.stylx).
+2. Define symbol attribute overrides (if any) using a collection of key-value pairs: `configured-attribute-name : override-attribute-name`.
+3. Define text attribute overrides (if any) using a collection of key-value pairs: `configured-attribute-name : override-attribute-name`.
+4. If necessary, provide new values for configuration settings defined in `DictionarySymbolStyle.Configurations`.
+5. Create a new `DictionaryRenderer`, providing the `DictionarySymbolStyle` and (optionally) the collection of symbol and text attribute overrides.
+6. Apply the dictionary renderer to a feature layer or graphics overlay with the expected attributes.
+
+## Relevant API
+
+* DictionaryRenderer
+* DictionarySymbolStyle
+* DictionarySymbolStyleConfiguration
+
+## About the data
+
+The custom [restaurant style file](https://arcgisruntime.maps.arcgis.com/home/item.html?id=751138a2e0844e06853522d54103222a) is downloaded automatically from an ArcGIS Online portal item. The symbols it contains were created using ArcGIS Pro. The logic used to apply the symbols comes from an Arcade script embedded in the stylx file (which is a SQLite database), along with a JSON string that defines expected attribute names and configuration properties. For information about creating your own custom dictionary style, see the open source [dictionary-renderer-toolkit](https://esriurl.com/DictionaryToolkit). 
+
+The style is applied to a dataset showing a subset of [restaurants in Redlands, CA](https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/Redlands_Restaurants/FeatureServer).
+
+## Tags
+
+dictionary, military, renderer, style, stylx, unique value, visualization
